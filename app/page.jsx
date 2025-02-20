d9ffd1ab7844787f1673b31b5b72069881021d52
Martina Best <martinabest@mac.home>
2025-02-20 16:37:03 +0000

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left ">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hi I'm <br /> <span className="text-accent">Martina Best</span>
@@ -46,7 +46,7 @@ const Home = () => {
           </div>
 
           {/* photo */}
-          <div>
+          <div className="order-1 xl:order-non mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,24 +1,39 @@
+
 "use client";
 import { motion } from "framer-motion";
-import Image from "next/image"; 
+import Image from "next/image";
 
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
-      <motion.div>
-        <div className="w-[298px] h-[398px] xl:w-[498px] xl:h-[598px] relative bg-transparent">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        {/* Adjust the size here */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[250px] h-[350px] xl:w-[400px] xl:h-[500px] mix-blend-lighten"
+        >
           <Image
             src="/MartinaPhoto.png"
             priority
             quality={100}
             fill
             alt="Martina"
-            className="object-contain bg-transparent"
+            className="object-contain"
           />
-        </div>
+        </motion.div>
       </motion.div>
     </div>
   );
-}
+};
 
 export default Photo;
