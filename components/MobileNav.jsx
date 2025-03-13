6061f1df2ef36695727b12a81eb07f5cbc054a8a
Martina Best <martinabest@mac.home>
2025-03-13 10:00:28 +0000

    nav bar/burger

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -4,6 +4,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 const links = [
   {
@@ -27,16 +28,23 @@ const links = [
     path: "/contact",
   },
 ];
+
 const MobileNav = () => {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setOpen(false); // Close the menu when a link is clicked
+  };
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <div className="mt-32 mb-40 text-center text 2xxl">
-          <Link href="/">
+          <Link href="/" onClick={handleLinkClick}>
             <h1 className="text-4xl font-semibold">
               Martina <span className="text-accent">.</span>
             </h1>
@@ -49,7 +57,11 @@ const MobileNav = () => {
               <Link
                 href={link.path}
                 key={index}
-                className={`${link.path === pathname && "text-accent border-b-2 border-accent"}text-xl capitalize hover:text-accent transition-all`}
+                onClick={handleLinkClick}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                }text-xl capitalize hover:text-accent transition-all`}
               >
                 {link.name}
               </Link>
